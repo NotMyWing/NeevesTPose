f83d5da44845e408f95164dc8841827576e06347
Neeve01 <winwyv@gmail.com>
2020-07-24 19:52:15 +0300

    Initial Commit

---
+++ gamemodes/terrortown/gamemode/lang/tpose.lua
@@ -0,0 +1,5 @@
+local L = LANG.CreateLanguage("English")
+L.tpose_switch = "Press {secondaryfire} to toggle the T-Pose"
+
+L = LANG.CreateLanguage("Русский")
+L.tpose_switch = "Нажмите {secondaryfire} для переключения Т-Позы"
